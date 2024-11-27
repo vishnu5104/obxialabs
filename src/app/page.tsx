90135b0a38beec4bd9d53709460ccf387e934bb2
vishnu5104 <vishnus510412@gmail.com>
2024-11-27 17:38:45 +0530

    shadcn components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import LiveHtmlEditor from '@/components/live-html-editor';
-
 export default function Page() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Live HTML Editor</h1>
-      <LiveHtmlEditor />
+      <h1 className="text-2xl font-bold mb-4">ObxiaLabs</h1>
     </div>
   );
 }
