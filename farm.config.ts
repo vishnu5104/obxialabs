afe32fa7ebe7892bb92c85f996488fea396034bc
vishnu5104 <vishnus510412@gmail.com>
2024-11-28 16:37:04 +0530

    configured with vite

--- farm.config.ts
+++
@@ -1,8 +0,0 @@
-// ...
-import { defineConfig } from "@farmfe/core";
-import farmPluginPostcss from "@farmfe/js-plugin-postcss";
-
-export default defineConfig({
-  // ... ignore other fields
-  plugins: ["@farmfe/plugin-react", "@farmfe/plugin-sass", farmPluginPostcss()],
-});
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <h1 className="bg-blue-400 text-3xl font-bold underline">Hello world!</h1>
+  );
+}
